d5e8797b98048f9b8efc6d85cfa9718949667c82
joelferzz <joel.zav@gmail.com>
2025-03-27 15:03:05 -0700

    Update test_calculadora.py

--- tests/test_calculadora.py
+++ tests/test_calculadora.py
@@ -1,7 +1,13 @@
 # tests/test_calculadora.py
 
 import pytest
+import sys
+import os
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from src.calculadora import Calculadora
+
 
 def test_sumar():
     calc = Calculadora()
